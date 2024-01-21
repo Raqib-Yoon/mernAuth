1b0d3d06c3bc60a4ae573d9cc3ab053ce9979f4a
Raqib-Yoon <raqibyoon2020@gmail.com>
2024-01-21 09:45:52 +0430

    Router page updated
    Router page updated

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,12 +3,14 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./core/Navbar";
 import Signup from "./auth/Signup";
 import Signin from "./auth/Signin";
+import ForgotPassword from "./auth/ForgotPassword";
 import App from "./App";
 import Activate from "./auth/Activate";
 import Protected from "./auth/Protected";
 import ProtectAdmin from "./auth/ProtectAdmin.jsx";
 import User from "./core/User";
 import Admin from "./core/Admin.jsx";
+import ResetPassword from "./auth/ResetPassword";
 const Router = ({ children }) => {
   return (
     <>
@@ -21,6 +23,8 @@ const Router = ({ children }) => {
           <Route path="/auth/activate/:token" element={<Activate />} />
           <Route path="/user" element={<Protected Component={User} />} />
           <Route path="/admin" element={<ProtectAdmin Admin={Admin} />} />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </>
